c47803d3eab947573728cde1208cc217b1bbab93
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-17 22:29:36 +0300

    test v0.21

--- app/test_main.py
+++ app/test_main.py
@@ -24,3 +24,11 @@ def test_valid_2():
     with mock.patch.object(datetime.date, "today") as mocked_today:
         mocked_today.return_value = datetime.date(2020, 1, 15)
         assert outdated_products(tested_list) == ["test item"]
+
+
+def test_valid_3(monkeypatch):
+    def mock_today():
+        return datetime.date(2020, 1, 15)
+
+    monkeypatch.setattr(datetime.date, "today", mock_today)
+    assert outdated_products(tested_list) == ["test item"]
