ac0794c60ddd529e9a5c7fde6d78915d5e5e1d62
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-17 21:10:16 +0300

    test v0.1

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from unittest import mock
+from app.main import outdated_products, datetime
+
+tested_list = [
+    {"name": "test item",
+     "expiration_date": datetime.date(2020, 1, 15),
+     "price": 100}
+]
+
+
+@mock.patch("datetime.date.today")
+def test_valid(mocked_today):
+    mocked_today.return_value = datetime.date(2020, 1, 15)
+    assert outdated_products(tested_list) == ["test item"]
+
+
+@mock.patch("datetime.date.today")
+def test_expired(mocked_today):
+    mocked_today.return_value = datetime.date(2020, 1, 16)
+    assert outdated_products(tested_list) is None
