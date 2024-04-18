bef2e173c06ff9e8f5b8dd2c998cd77af62dd74d
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-18 19:34:53 +0300

    test v0.23

--- app/test_main.py
+++ app/test_main.py
@@ -10,25 +10,25 @@ tested_list = [
 
 @mock.patch("datetime.date.today")
 def test_valid(mocked_today: mock.patch) -> None:
-    mocked_today.return_value = datetime.date(2020, 1, 15)
+    mocked_today.return_value = datetime.date(2020, 1, 14)
     assert outdated_products(tested_list) == ["test item"]
 
 
 @mock.patch.object(datetime.date, "today")
 def test_valid_1(mocked_today: mock.patch.object) -> None:
-    mocked_today.return_value = datetime.date(2020, 1, 15)
+    mocked_today.return_value = datetime.date(2020, 1, 14)
     assert outdated_products(tested_list) == ["test item"]
 
 
 def test_valid_2() -> None:
     with mock.patch.object(datetime.date, "today") as mocked_today:
-        mocked_today.return_value = datetime.date(2020, 1, 15)
+        mocked_today.return_value = datetime.date(2020, 1, 14)
         assert outdated_products(tested_list) == ["test item"]
 
 
 def test_valid_3(monkeypatch: mock) -> None:
     def mock_today() -> datetime:
-        return datetime.date(2020, 1, 15)
+        return datetime.date(2020, 1, 14)
 
     monkeypatch.setattr(datetime.date, "today", mock_today)
     assert outdated_products(tested_list) == ["test item"]
