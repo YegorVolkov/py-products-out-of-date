02c18954e55839bbc3388d08b5001c29b8cb0486
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-19 20:44:45 +0300

    test v1.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,34 +1,19 @@
-from unittest import mock
 from app.main import outdated_products, datetime
+from unittest import mock
+
 
 tested_list = [
-    {"name": "test item",
+    {"name": "valid item",
+     "expiration_date": datetime.date(2020, 1, 14),
+     "price": 100},
+    {"name": "expired item",
      "expiration_date": datetime.date(2020, 1, 15),
-     "price": 100}
+     "price": 100},
 ]
 
 
-@mock.patch("datetime.date.today")
-def test_valid(mocked_today: mock.patch) -> None:
-    mocked_today.return_value = datetime.date(2020, 1, 14)
-    assert outdated_products(tested_list) == ["test item"]
-
-
-@mock.patch.object(datetime.date, "today")
-def test_valid_1(mocked_today: mock.patch.object) -> None:
-    mocked_today.return_value = datetime.date(2020, 1, 14)
-    assert outdated_products(tested_list) == ["test item"]
-
-
-def test_valid_2() -> None:
-    with mock.patch.object(datetime.date, "today") as mocked_today:
-        mocked_today.return_value = datetime.date(2020, 1, 14)
-        assert outdated_products(tested_list) == ["test item"]
-
-
-def test_valid_3(monkeypatch: mock) -> None:
-    def mock_today() -> datetime:
-        return datetime.date(2020, 1, 14)
-
-    monkeypatch.setattr(datetime.date, "today", mock_today)
-    assert outdated_products(tested_list) == ["test item"]
+def test_valid() -> None:
+    with mock.patch("app.main.datetime") as mocked_datetime:
+        today = datetime.date(2020, 1, 15)
+        mocked_datetime.date.today.return_value = today
+        assert outdated_products(tested_list) == ["valid item"]
